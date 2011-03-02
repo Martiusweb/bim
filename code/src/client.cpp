f377a4b65e4bbb4942767f6f330e295b580ec103
Martin Richard <martius@martiusweb.net>
2011-03-02 20:57:29 +0100

    wrong headers on some files

--- code/src/client.cpp
+++ code/src/client.cpp
@@ -30,7 +30,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * File author : Paul ADENOT <paul@paul.cx>
+ * File author : Martin Richard <martius@martiusweb.net>
  *
  **/
 
