c3697c274879fd5538047b9c834998f6d05c7b95
Paul ADENOT <paul@paul.cx>
2011-04-02 17:54:23 +0200

    various changes

--- code/epoll_server.c
+++ code/epoll_server.c
@@ -1,4 +1,5 @@
 /**
+ * 
  * A simple server implemented using e-poll.
  */
 #include <sys/types.h>
@@ -11,6 +12,10 @@
 #include <stdio.h>
 #include <sys/epoll.h>
 #include <cstring>
+#include <iostream>
+
+#define var_dump(x) std::cerr << #x << ":" << x << std::endl;
+
 
 /*** Configuration ***/
 /* Server port */
@@ -19,7 +24,7 @@ static const uint16_t SERVER_PORT = 7001;
 static const int MAX_CLIENTS = 1000;
 /* Max epoll events that can be caught
  * TODO explain how to choose this value (currently *rand*) */
-static const int MAX_EPOLL_EVENTS = 64;
+static const int MAX_EPOLL_EVENTS = 100;
 /* Max epoll events processed per loop
  * TODO explain this one too */
 static const int MAX_EPOLL_EVENTS_PER_LOOP = 16;
@@ -35,25 +40,18 @@ typedef struct {
  */
 static int socket_set_nonblocking(int fd)
 {
-    int socket_flags = 1;
-    /* Reuse address : tells the kernel that even if this port is busy (in the
-       TIME_WAIT state), go ahead and reuse it anyway. Avoid "Already in use"
-       error when restarting the program.  */
-    if(setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &socket_flags, sizeof(int))
-            < 0) {
-        return -1;
-    }
+  int socket_flags = 1;
 
-    socket_flags = 0;
-    if((socket_flags = fcntl(fd, F_GETFL, 0)) == -1) {
-        return -1;
-    }
+  socket_flags = 0;
+  if((socket_flags = fcntl(fd, F_GETFL, 0)) == -1) {
+    return -1;
+  }
 
-    if(fcntl(fd, F_SETFL, socket_flags|O_NONBLOCK) == -1) {
-        return -1;
-    }
+  if(fcntl(fd, F_SETFL, socket_flags|O_NONBLOCK) == -1) {
+    return -1;
+  }
 
-    return fd;
+  return fd;
 }
 
 /**
@@ -64,34 +62,43 @@ static int socket_set_nonblocking(int fd)
  */
 static int create_server(uint16_t port, int max_clients)
 {
-    int socket_server, epollq;
-    struct sockaddr_in addr_server = {0}; 
+  int socket_server, epollq;
+  int socket_flags = 1;
+  struct sockaddr_in addr_server = {0}; 
 
-    /* Initialize the server socket */
-    socket_server = socket(AF_INET, SOCK_STREAM, 0);
-    if(socket_server == -1) {
-        perror("Server socket can not be created ");
-        return -1;
-    }
+  /* Initialize the server socket */
+  socket_server = socket(AF_INET, SOCK_STREAM, 0);
+  if(socket_server == -1) {
+    perror("Server socket can not be created ");
+    return -1;
+  }
 
-    addr_server.sin_family = AF_INET;
-    addr_server.sin_port = htons(port);
-    addr_server.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr_server.sin_family = AF_INET;
+  addr_server.sin_port = htons(port);
+  addr_server.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    if(bind(socket_server, (struct sockaddr*) &addr_server,
-                sizeof(addr_server)) != 0) {
-        close(socket_server);
-        perror("Can not bind the socket ");
-        return -1;
-    }
+  /* Reuse address : tells the kernel that even if this port is busy (in the
+     TIME_WAIT state), go ahead and reuse it anyway. Avoid "Already in use"
+     error when restarting the program.  */
+  if(setsockopt(socket_server , SOL_SOCKET, SO_REUSEADDR, &socket_flags, sizeof(int))
+      < 0) {
+    return -1;
+  }
 
-    if(listen(socket_server, max_clients) < 0) {
-        close(socket_server);
-        perror("Can not listen on this interface ");
-        return -1;
-    }
+  if(bind(socket_server, (struct sockaddr*) &addr_server,
+        sizeof(addr_server)) != 0) {
+    close(socket_server);
+    perror("Can not bind the socket ");
+    return -1;
+  }
 
-    return socket_server;
+  if(listen(socket_server, max_clients) < 0) {
+    close(socket_server);
+    perror("Can not listen on this interface ");
+    return -1;
+  }
+
+  return socket_server;
 }
 
 /**
@@ -102,21 +109,22 @@ static int create_server(uint16_t port, int max_clients)
  * Returns the epoll fd or -1 in case of error
  */
 static int epoll_watch_in(int epollq, int socket_server, int mode =
-        EPOLL_CTL_ADD)
+    EPOLL_CTL_ADD)
 {
-    struct epoll_event event;
+  struct epoll_event event;
 
-    /* Nb events EPOLLHUP and EPOLLERR are impicitly set */
-    event.events = EPOLLIN /* ready to read */ | EPOLLET /* Edge Triggered */;
-    bim_connection_t* c = new bim_connection_t;
-    c->fd = socket_server;
-    event.data.ptr = (void*)c;
+  /* Nb events EPOLLHUP and EPOLLERR are impicitly set */
+  event.events = EPOLLIN /* ready to read */ /* | EPOLLET */ /* Edge Triggered */;
+  bim_connection_t* c = new bim_connection_t;
+  c->fd = socket_server;
+  event.data.ptr = (void*)c;
 
-    if(epoll_ctl(epollq, mode, socket_server, &event) == -1) {
-        return -1;
-    }
+  if(epoll_ctl(epollq, mode, socket_server, &event) == -1) {
+    std::cerr << "epoll ctl fail" << std::endl;
+    return -1;
+  }
 
-    return epollq;
+  return epollq;
 }
 
 /**
@@ -127,24 +135,26 @@ static int epoll_watch_in(int epollq, int socket_server, int mode =
  * Returns the epoll fd or -1 in case of error
  */
 static int epoll_watch_inout(int epollq, int socket_client, int mode =
-        EPOLL_CTL_ADD)
+    EPOLL_CTL_ADD)
 {
-    struct epoll_event event;
+  struct epoll_event event;
 
-    /* @see epoll_watch_in */
-    event.events = EPOLLIN | EPOLLOUT /* ready to write */ | EPOLLET;
+  /* @see epoll_watch_in */
+  event.events = EPOLLIN | EPOLLOUT /* ready to write */ | EPOLLET;
 
-    bim_connection_t* c = new bim_connection_t;
-    c->fd = socket_client;
-    c->string = new char[21];
-    strcpy(c->string, "persistant");
+  bim_connection_t* c = new bim_connection_t;
+  c->fd = socket_client;
+  c->string = new char[21];
+  strcpy(c->string, "persistant");
 
-    event.data.ptr = (void*) c;
+  event.data.ptr = (void*) c;
 
-    if(epoll_ctl(epollq, mode, socket_client, &event) == -1) {
-        return -1;
-    }
-    return epollq;
+  if(epoll_ctl(epollq, mode, socket_client, &event) == -1) {
+    std::cerr << "epoll_ctl failed" << std::endl;
+    exit(1);
+    return -1;
+  }
+  return epollq;
 }
 
 /**
@@ -155,12 +165,14 @@ static int epoll_watch_inout(int epollq, int socket_client, int mode =
  */
 static int epoll_unwatch(int epollq, int fd)
 {
-    /* We could be bitches and use a static structure */
-    struct epoll_event event;
-    if(epoll_ctl(epollq, EPOLL_CTL_DEL, fd, &event) == -1) {
-        return -1;
-    }
-    return epollq;
+  /* We could be bitches and use a static structure */
+  struct epoll_event event;
+  if(epoll_ctl(epollq, EPOLL_CTL_DEL, fd, &event) == -1) {
+    std::cerr << "epoll_ctl failed" << std::endl;
+    exit(-1);
+    return -1;
+  }
+  return epollq;
 }
 
 /**
@@ -168,94 +180,137 @@ static int epoll_unwatch(int epollq, int fd)
  */
 int main()
 {
-    int socket_server = 0, socket_client = 0, epollq = 0, nb_events = 0,
-        read = 0, i = 0;
-    socklen_t addr_ln = 0;
-    struct epoll_event epoll_event, events[MAX_EPOLL_EVENTS_PER_LOOP];
-    /* TODO it may be good to keep the user address somewhere... */
-    struct sockaddr_in addr_client = {0};
+  int socket_server = 0, socket_client = 0, epollq = 0, nb_events = 0,
+      read = 0, i = 0;
+  socklen_t addr_ln = 0;
+  struct epoll_event epoll_event, events[MAX_EPOLL_EVENTS_PER_LOOP];
+  /* TODO it may be good to keep the user address somewhere... */
+  struct sockaddr_in addr_client = {0};
 
-    /* For testing purpose : implementation quite stupid of a dummy protocol */
-    int BUFFER_SIZE = 256;
-    char buffer[BUFFER_SIZE];
+  /* For testing purpose : implementation quite stupid of a dummy protocol */
+  int BUFFER_SIZE = 256;
+  char buffer[BUFFER_SIZE];
 
-    if((socket_server = create_server(SERVER_PORT, MAX_CLIENTS)) == -1) {
-        perror("Can not create or bind or listen on the server socket ");
-        exit(errno);
-    }
-    printf("Listening to %d\n", SERVER_PORT);
+  if((socket_server = create_server(SERVER_PORT, MAX_CLIENTS)) == -1) {
+    perror("Can not create or bind or listen on the server socket ");
+    exit(errno);
+  }
+  printf("Listening to %d\n", SERVER_PORT);
 
-    if((epollq = epoll_create(MAX_EPOLL_EVENTS)) == -1) {
-        close(socket_server);
-        perror("Can not create epoll object ");
-        exit(errno);
-    }
-    printf("Epoll ready to wait for %d events !\n", MAX_EPOLL_EVENTS);
+  if((epollq = epoll_create(MAX_EPOLL_EVENTS)) == -1) {
+    close(socket_server);
+    perror("Can not create epoll object ");
+    exit(errno);
+  }
+  printf("Epoll ready to wait for %d events !\n", MAX_EPOLL_EVENTS);
 
-    if(epoll_watch_in(epollq, socket_server) == -1) {
-        close(epollq);
-        close(socket_server);
-        perror("Can not add the server socket file descriptor to epoll ");
-        exit(errno);
-    }
-
-    /* Nao, our event loop */
-    for(;;)
-    {
-        /* Blocking wait, epoll will store at least */
-        nb_events = epoll_wait(epollq, events, MAX_EPOLL_EVENTS_PER_LOOP, -1);
-
-        for(i = 0; i < nb_events; ++i)
-        {
-            /** Unpack the structure **/
-            bim_connection_t* c = (bim_connection_t*) events[i].data.ptr;
-            /* error on a client socket */
-            if(c->fd != socket_server &&
-                    (events[i].events & (EPOLLHUP|EPOLLERR))) {
-                perror("Connection with client closed\n");
-                epoll_unwatch(epollq, c->fd);
-                close(c->fd);
-            }
-            else if(c->fd == socket_server && (events[i].events &
-                        EPOLLIN)) {
-                if((socket_client = accept(socket_server, (struct sockaddr*)
-                                &addr_client, &addr_ln)) > 0) {
-
-                    if(socket_set_nonblocking(socket_client) == -1) {
-                        close(socket_client);
-                        perror("Can not set the client non blocking\n");
-                    }
-                    else if(epoll_watch_in(epollq, socket_client) == -1) {
-                        close(socket_client);
-                        perror("Can not watch the client with epoll\n");
-                    }
-                }
-            }
-            else {
-                if(events[i].events & EPOLLIN) {
-                    read = recv(c->fd, buffer, BUFFER_SIZE,
-                            MSG_DONTWAIT);
-                    epoll_watch_inout(epollq, c->fd,
-                            EPOLL_CTL_MOD);
-                    buffer[read] = 0;
-                    printf("Read from socket \"%s\"\n", buffer);
-                }
-                if(events[i].events & EPOLLOUT) {
-                    printf("persistant data ? %s\n", c->string);
-                    send(c->fd, "plop\n", 5,
-                            MSG_DONTWAIT|MSG_NOSIGNAL);
-                    epoll_unwatch(epollq, c->fd);
-                    close(c->fd);
-                    delete [] c->string;
-                    delete c;
-                }
-            }
-        }
-    }
-
+  if(epoll_watch_in(epollq, socket_server) == -1) {
     close(epollq);
     close(socket_server);
-    printf("Good bye !\n");
-    return 0;
+    perror("Can not add the server socket file descriptor to epoll ");
+    exit(errno);
+  }
+
+  /* Nao, our event loop */
+  for(;;)
+  {
+    nb_events = epoll_wait(epollq, events, MAX_EPOLL_EVENTS_PER_LOOP, -1);
+
+    if(nb_events == -1)
+    {
+      std::cerr << "nb_events == -1" << std::endl;
+      exit(1);
+    }
+
+    for(i = 0; i < nb_events; ++i)
+    {
+      /** Unpack the structure **/
+      bim_connection_t* c = (bim_connection_t*) events[i].data.ptr;
+      /* error on a client socket */
+      if(c->fd != socket_server && (events[i].events & (EPOLLHUP|EPOLLERR)))
+      {
+        perror("Connection with client closed\n");
+        epoll_unwatch(epollq, c->fd);
+        delete c->string;
+        close(c->fd);
+      }
+      else if(c->fd == socket_server && (events[i].events & EPOLLIN)) 
+      {
+        int rv = 0;
+        static int accepted = 0;
+        int count = 0;
+        do 
+        {
+        if((socket_client = accept(socket_server, (struct sockaddr*) &addr_client, &addr_ln)) > 0)
+        {
+          count++;
+          accepted++;
+          var_dump(accepted);
+          if(socket_set_nonblocking(socket_client) == -1) {
+            close(socket_client);
+            perror("Can not set the client non blocking\n");
+          }
+          else if(epoll_watch_inout(epollq, socket_client) == -1) {
+            close(socket_client);
+            perror("Can not watch the client with epoll\n");
+          }
+        }
+        std::cerr << "new client" << std::endl;
+        } while(rv =! -1 && errno == EAGAIN);
+      }
+      else {
+        if(events[i].events & EPOLLIN) 
+        {
+          int received = 0;
+          do 
+          {
+            received = recv(c->fd, buffer, BUFFER_SIZE, MSG_DONTWAIT);
+            if(received == -1 && errno != EAGAIN)
+            {
+              perror("recv");
+              exit(-1);
+            }
+
+            static int read = 0;
+            read++;
+            var_dump(read);
+          } while(received != -1 && errno != EAGAIN);
+          std::cerr << "Got EAGAIN, exiting loop" << std::endl;
+        }
+        if(events[i].events & EPOLLOUT) {
+          int sent = 0;
+          const char* resp = "HTTP/1.0 200 OK\r\nContent-Length: 4\r\nConnection: close\r\n\r\nplop\r\n";
+          do 
+          {
+            sent = send(c->fd, resp, strlen(resp), MSG_DONTWAIT|MSG_NOSIGNAL);
+            if(sent == -1 && errno != EAGAIN)
+            {
+              perror("send");
+              exit(1);
+            }
+
+          } while(sent != strlen(resp));
+
+          // epoll_unwatch(epollq, c->fd);
+          if(close(c->fd))
+          {
+            perror("close");
+            exit(1);
+          }
+          delete [] c->string;
+          delete c;
+
+          static int written = 0;
+          written++;
+          var_dump(written);
+        }
+      }
+    }
+  }
+
+  close(epollq);
+  close(socket_server);
+  printf("Good bye !\n");
+  return 0;
 }
 
